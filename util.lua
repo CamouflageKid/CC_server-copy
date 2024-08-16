74a707f02ca5fad12d56a0ffb4a07b39d33cc0ec
That guy on the internet. <99547923+CamouflageKid@users.noreply.github.com>
2024-08-16 15:21:31 -0400

    update

--- util.lua
+++ util.lua
@@ -1,5 +1,28 @@
+local function output(dir)
+    redstone.setOutput("left", false)
+    redstone.setOutput("right", false)
+    redstone.setOutput("front", false)
+    redstone.setOutput("back", false)
+
+    redstone.setOutput(dir, true)
+end
+
+
 while true do
-    redstone.setOutput("top", not redstone.getOutput("top"))
-    print(redstone.getOutput("top"))
-    sleep(0.5)
+
+    local val = math.random(0, 4)
+
+
+    if val < 1 then
+        output("right")
+    elseif val >= 1 and val < 2 then
+        output("left")
+    elseif val >= 2 and val < 3 then
+        output("front")
+    elseif val >= 3 and val <= 4 then
+        output("back")
+    end
+
+    
+    sleep(1)
 end
