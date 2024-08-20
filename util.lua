14373de97c00d102190958ebc0d01d3f1b971388
That guy on the internet. <99547923+CamouflageKid@users.noreply.github.com>
2024-08-20 12:21:12 -0400

    update

--- util.lua
+++ util.lua
@@ -23,25 +23,44 @@ local function compare_locations(p1,p2,p3)
     return false
 end
 
-while true do
+Random_val = true
 
+function Rednet_FAdmin()
+    id, data = rednet.receive("forest_admin")
 
-
-
-
-    local val = math.random(0, 4)
-
-
-    if val < 1 then
-        output("right")
-    elseif val >= 1 and val < 2 then
-        output("left")
-    elseif val >= 2 and val < 3 then
-        output("front")
-    elseif val >= 3 and val <= 4 then
-        output("back")
+    if compare_locations(data.p1, data.p2, data.p3) then
+        Random_val = false
+        redstone.setOutput("left", true)
+        redstone.setOutput("right", true)
+        redstone.setOutput("front", true)
+        redstone.setOutput("back", true)
+    else
+        Random_val = true
     end
+end
 
+
+
+function Main()
+    while true do
+        sleep(0.05)
+        if Random_val then
     
-    sleep(10)
+            local val = math.random(0, 4)
+
+
+            if val < 1 then
+                output("right")
+            elseif val >= 1 and val < 2 then
+                output("left")
+            elseif val >= 2 and val < 3 then
+                output("front")
+            elseif val >= 3 and val <= 4 then
+                output("back")
+            end
+
+            
+            sleep(10)
+        end
+    end
 end
