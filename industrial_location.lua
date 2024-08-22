02a29e03509f5c5718c621c0d4a00bb6f9e1c4de
That guy on the internet. <99547923+CamouflageKid@users.noreply.github.com>
2024-08-22 13:29:28 -0400

    update

--- industrial_location.lua
+++ industrial_location.lua
@@ -11,6 +11,7 @@ local function output(dir)
     redstone.setOutput(dir, true)
 end
 
+Player_List = {}
 Range = 100
 local pos1, pos2, pos3 = gps.locate()
 
@@ -23,29 +24,52 @@ local function compare_locations(p1,p2,p3)
     return false
 end
 
+local function check()
+    local is_player_in_area = false
+    for i, v in pairs(Player_List) do
+        local checked = compare_locations(v.p1, v.p2, v.p3)
+        if checked then is_player_in_area = true end
+    end
+    return is_player_in_area
+end
+
 function Rednet_FAdmin()
     while true do
         id, data = rednet.receive("industrial_admin", 1)
 
         if data then
-            if compare_locations(data.p1, data.p2, data.p3) then
-                Random_val = false
-                redstone.setOutput("left", false)
-                redstone.setOutput("right", false)
-                redstone.setOutput("front", false)
-                redstone.setOutput("back", false)
-            else
-                redstone.setOutput("left", true)
-                redstone.setOutput("right", true)
-                redstone.setOutput("front", true)
-                redstone.setOutput("back", true)
-            end
+            Player_List[#Player_List + 1] = data
+            --if compare_locations(data.p1, data.p2, data.p3) then
+            --    Random_val = false
+            --    redstone.setOutput("left", false)
+            --    redstone.setOutput("right", false)
+            --    redstone.setOutput("front", false)
+            --    redstone.setOutput("back", false)
+            --else
+            --    redstone.setOutput("left", true)
+            --    redstone.setOutput("right", true)
+            --    redstone.setOutput("front", true)
+            --    redstone.setOutput("back", true)
+            --end
+        else
+            --redstone.setOutput("left", true)
+            --redstone.setOutput("right", true)
+            --redstone.setOutput("front", true)
+            --redstone.setOutput("back", true)
+        end
+
+        if check() then
+            redstone.setOutput("left", false)
+            redstone.setOutput("right", false)
+            redstone.setOutput("front", false)
+            redstone.setOutput("back", false)
         else
             redstone.setOutput("left", true)
             redstone.setOutput("right", true)
             redstone.setOutput("front", true)
             redstone.setOutput("back", true)
         end
+
     end
 end
 
